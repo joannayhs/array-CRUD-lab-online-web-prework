bbce1e2f341c981ba221c39de8bdc04a32b60836
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-19 00:14:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Wow", "I", "Am","Really","Learning"]
-  array << "Arrays!"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)

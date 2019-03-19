e3e0a79341943b34203bc0d926337fcd5f3eb0f3
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-19 00:09:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  friends << "Peter"
+  family = ["mom", "dad"]
+  family << "Vicki"
 end
 
 def add_element_to_start_of_array(array, element)
